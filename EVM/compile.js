69984b0f20ef79adc73cbdd908254c8f1ace8b7a
Sean Pollock <seanpollock3344@gmail.com>
2017-03-21 21:27:18 -0500

    Re-structuring compile methods.

--- EVM/compile.js
+++ EVM/compile.js
@@ -2,24 +2,27 @@ import Promise from 'bluebird';
 import Eth from 'ethjs';
 import fs from 'fs';
 
-Promise.delay(0)
-.then(() => {
-  return getContractFiles();
-}).map((file) => {
-  console.log('file');
-}).catch((error) => {
-  console.log(error);
-})
+export function compile() {
 
-// utility functions
+}
 
-function getContractFiles() {
+export function getContractFiles() {
   return new Promise((resolve, reject) => {
     let files = [];
     fs.readdir('./contracts')
     .map((file) => {
       console.log(file);
       files.push(file)
+    }).then(() => {
+      resolve(files);
+    }).catch((error) => {
+      reject(error);
     })
   })
 }
+
+export function getContracts() {
+
+}
+
+export function
