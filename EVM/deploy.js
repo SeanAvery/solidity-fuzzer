dcf2c213e575a73167e7d088596371e7d7a08224
Sean Pollock <seanpollock3344@gmail.com>
2017-03-22 13:44:57 -0500

    Instantiating ethjs provider

--- EVM/deploy.js
+++ EVM/deploy.js
@@ -2,7 +2,30 @@ import Promise from 'bluebird';
 import Eth from 'ethjs';
 import fs from 'fs';
 
-Promise.delay(0)
-.then(() => {
-    console.log('reading contracts directory');
-})
+let HttpProvider;
+let eth;
+
+export function deploy() {
+  return new Promise((resolve, reject) => {
+    Promise.resolve(web3Provider())
+    .then(() => {
+
+    }).catch((error) => {
+      reject(error);
+    });
+  });
+}
+
+export function web3Provider() {
+  return new Promise((resolve, reject) => {
+    Promise.delay(0)
+    .then(() => {
+      eth = new Eth(new Eth.HttpProvider('http://localhost:8545'));
+      console.log('eth', eth);
+    }).catch((error) => {
+      reject(error);
+    })
+  });
+}
+
+deploy();
