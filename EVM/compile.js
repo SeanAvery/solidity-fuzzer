69fe7d4402c0b6181f351c631580eac3704a7e1c
Sean Pollock <seanpollock3344@gmail.com>
2017-03-21 22:15:00 -0500

    Promisifying all fs calls.

--- EVM/compile.js
+++ EVM/compile.js
@@ -1,15 +1,20 @@
 import Promise from 'bluebird';
 import Eth from 'ethjs';
-import fs from 'fs';
 
+const fs = Promise.promisifyAll(require('fs'));
 export function compile() {
-
+  return new Promise((resolve, reject) => {
+    Promise.resolve(getContractFiles())
+    .map((file) => {
+      console.log('file', file);
+    })
+  })
 }
 
 export function getContractFiles() {
   return new Promise((resolve, reject) => {
     let files = [];
-    fs.readdir('./contracts')
+    Promise.resolve(fs.readdir('/contracts'))
     .map((file) => {
       console.log(file);
       files.push(file)
@@ -25,4 +30,4 @@ export function getContracts() {
 
 }
 
-export function
+compile();
